02b2293740b7d77d86bca5878335474dad7a20c4
Joe Howard <succinction@gmail.com>
2019-03-31 11:50:13 -0700

    REX-100 first circle

--- src/index.js
+++ src/index.js
@@ -4,6 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+
+function reducer(state, action) {
+    if (action.type === 'changeState') {
+        return action.payload.newState
+    }
+    return 'State'
+}
+
+const store = createStore(reducer)
+
+
+console.log(store.getState())
+
+
+const action = {
+    type: 'changeState',
+    payload: {
+        newState: 'New state'
+    }
+}
+store.dispatch(action)
+
+
+console.log(store.getState())
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
