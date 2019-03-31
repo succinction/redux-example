92e029a15c016f356f33705818813c0c478584f7
Joe Howard <succinction@gmail.com>
2019-03-31 12:27:42 -0700

    REX-100 evolution one

--- src/index.js
+++ src/index.js
@@ -3,37 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-import { createStore } from 'redux';
-
-function reducer(state, action) {
-    if (action.type === 'changeState') {
-        return action.payload.newState
-    }
-    return 'State'
+function productReducer(state = [], action) {
+    return state
 }
 
-const store = createStore(reducer)
+function userReducer(state = '', { type, payload }) {
+    switch (type) {
+        case 'updateUser':
+            return payload;
+    }
+    return state
+}
 
+const allReducers = combineReducers({
+    products: productReducer,
+    user: userReducer
+})
+
+const store = createStore(
+    allReducers,
+    {
+        products: [{ name: 'iPhone' }],
+        user: 'Michael'
+    },
+    window.devToolsExtension && window.devToolsExtension()
+)
 
 console.log(store.getState())
 
-
-const action = {
-    type: 'changeState',
-    payload: {
-        newState: 'New state'
-    }
+const updateUserAction = {
+    type: 'updateUser',
+    payload: "Joe",
 }
-store.dispatch(action)
 
+store.dispatch(updateUserAction)
 
 console.log(store.getState())
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+/////////////////////////////////////////////////////////////////////////////////////////////////
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
