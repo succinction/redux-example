eb1041ba101e8248c6a74beeb0fc509ecd752952
Joe Howard <succinction@gmail.com>
2019-03-31 19:58:12 -0700

    REX-100 add reselect

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux';
 import { apiRequest, updateUser } from './actions/userActions'
+import { createSelector } from 'reselect';
+
 
 class App extends Component {
   constructor(props) {
@@ -14,7 +16,7 @@ class App extends Component {
   componentDidMount() {
     setTimeout(
       this.props.onApiRequest,
-      2300
+      1300
     )
   }
 
@@ -28,7 +30,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-
         </header>
         <div>
           Update User: <br />
@@ -40,22 +41,46 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = (state, props) => {
-  return {
-    products: state.products,
-    user: state.user,
-    usePlusProps: `${state.user} ${props.aRandomProp}`
-  }
-}
+const productSelector = createSelector(
+  state => state.products,
+  products => products
+)
+
+const userSelector = createSelector(
+  state => state.user,
+  user => user
+)
+
+const mapStateToProps = createSelector(
+  productSelector,
+  userSelector,
+  (products, user) => ({
+    products, 
+    user
+  })
+)
 
 const mapActionsToProps = {
   onUpdateUser: updateUser,
   onApiRequest: apiRequest,
 }
 
+export default connect(mapStateToProps, mapActionsToProps)(App);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// const mapStateToProps = (state, props) => {
+//   return {
+//     products: state.products,
+//     user: state.user,
+//     usePlusProps: `${state.user} ${props.aRandomProp}`
+//   }
+// }
+
+
 // const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
 //   console.log(propsFromState, propsFromDispatch, ownProps)
 //   return {}
 // }
-
-export default connect(mapStateToProps, mapActionsToProps)(App);
